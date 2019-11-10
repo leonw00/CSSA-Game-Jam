798cd132ba9729926149c4e009f54be58343fe46
leonw00 <47224043+leonw00@users.noreply.github.com>
2019-11-09 19:42:38 -0600

    Add Things

---
+++ Assets/_Scripts/ObstacleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+
+    public float moveSpeed; 
+
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int cooldown = 2;
+
+    private void Start()
+    {
+        
+    }
+
+}
