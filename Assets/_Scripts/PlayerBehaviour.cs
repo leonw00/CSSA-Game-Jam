223f4a085a396bf0f777e2129df260fc7c36edb0
leonw00 <47224043+leonw00@users.noreply.github.com>
2019-11-09 20:11:07 -0600

    Added Things

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    private Rigidbody2D playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertial"), 0);
+    }
+}
--- Assets/_Scripts/Spawner.cs
+++ Assets/_Scripts/Spawner.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public int cooldown = 2;
+    public double startCooldown = 2;
+    public int leftBoundary = -7;
+    public int rightBoundary = 12;
+    private double cooldown;
+
+    public GameObject obstacle;
 
     private void Start()
     {
-        
+        cooldown = startCooldown;
+    }
+
+    private void Update()
+    {
+        if(cooldown < 0)
+        {
+            Instantiate(obstacle, new Vector3(Random.Range(leftBoundary, rightBoundary), transform.position.y, transform.position.z), Quaternion.identity);
+            cooldown = startCooldown;
+            startCooldown -= 0.02;
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+        }
     }
 
 }
