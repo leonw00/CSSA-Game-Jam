5ea306935e11277128a306b0a096d0c83fe562a5
leonw00 <47224043+leonw00@users.noreply.github.com>
2019-11-10 14:38:31 -0600

    Add

--- Assets/_Scripts/Destroyer.cs
+++ Assets/_Scripts/Destroyer.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
 
+    public Text txt;
+    private int score = 0;
+     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Obstacle"))
         {
+            score++;
             Destroy(collision.gameObject);
         }
+    }
+
+    public void Start()
+    {
+        txt = gameObject.GetComponent<Text>();
+        txt.text = "Score: " + score;
+    }
+
+    public void Update()
+    {
+        txt.text = "Score: " + score;
     }
 
 }
